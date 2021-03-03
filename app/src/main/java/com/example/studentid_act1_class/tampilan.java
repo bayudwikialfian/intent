87633a183a1e12075df2bc962726d9b8882a8b2f
bayudwikialfian <bayudwiki@gmail.com>
2021-03-03 15:59:16 +0700

    sukses di tampikan awal

---
+++ app/src/main/java/com/example/studentid_act1_class/tampilan.java
@@ -0,0 +1,14 @@
+package com.example.studentid_act1_class;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tampilan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan);
+    }
+}
