d53f64a6035335e058a975cd1cf11e728bd7322d
bayudwikialfian <bayudwiki@gmail.com>
2021-03-09 12:57:53 +0700

    sukses di tampikan awal

--- app/src/main/java/com/example/studentid_act1_class/MainActivity.java
+++ app/src/main/java/com/example/studentid_act1_class/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.studentid_act1_class;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ETusername, ETpassword;
+    Button btnmasuk;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan);
+
+        ETusername = findViewById(R.id.ETusername);
+        ETpassword = findViewById(R.id.ETpassword);
+        btnmasuk = findViewById(R.id.masuk);
+
+
+        btnmasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child("login");
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        String username = ETusername.getText().toString();
+                        String password = ETpassword.getText().toString();
+
+                        if (snapshot.child(username).exists()) {
+                            if (snapshot.child(password).exists()) {
+
+                            } else {
+
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "kata sandi tidak tersedia", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+
+                    ;
+                });
+
+            }
+
+        });
     }
 }
